cd019d89dacecdb79875b82645c8397b8b1fec13
Reece Holland <reece.j.holland@gmail.com>
2026-05-22 06:57:34 +0800

    added unit tests for fault config schema

---
+++ test/test_fault_config_schema.cpp
@@ -0,0 +1,24 @@
+#include "ros2_fault_injection/fault_config_schema.hpp"
+
+#include <gtest/gtest.h>
+
+#include "ros2_fault_injection/fault_config.hpp"
+#include "ros2_fault_injection/scenario_config.hpp"
+
+namespace ros2_fault_injection {
+
+TEST(FaultConfigSchema, AcceptsValidFaultConfig) {
+  EXPECT_TRUE(is_allowed_config_key("odom", "x_bias"));
+  EXPECT_TRUE(is_allowed_config_key("odom", "drop_probability"));
+  EXPECT_TRUE(is_allowed_config_key("scan", "range_bias"));
+  EXPECT_TRUE(is_allowed_config_key("joint_state", "velocity_bias"));
+}
+
+TEST(FaultConfigSchema, RejectsInvalidFaultConfig) {
+  EXPECT_FALSE(is_allowed_config_key("odom", "range_bias"));
+  EXPECT_FALSE(is_allowed_config_key("scan", "velocity_bias"));
+  EXPECT_FALSE(is_allowed_config_key("joint_state", "x_bias"));
+  EXPECT_FALSE(is_allowed_config_key("unknown_injector", "some_key"));
+}
+
+}  // namespace ros2_fault_injection
